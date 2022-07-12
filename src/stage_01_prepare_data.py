79ca5f2d4d715524e9cd33a890a8f7ee7ff776b8
Sarika Mohanraj <sarikamohan08@gmail.com>
2022-07-12 08:47:12 +0530

    tsv created

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -1,14 +1,12 @@
 import argparse
 import os
-import shutil
-from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
-STAGE = "Prepare data" ## <<< change stage name 
-
+STAGE = "Prepare_data" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -22,8 +20,29 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    source_data_dir = config["source_data"]["data_dir"]
+    source_data_file = config["source_data"]["data_file"]
+    source_data_path = os.path.join(source_data_dir, source_data_file)
 
+    split = params["prepare"]["split"] # split ratio
+    seed = params["prepare"]["seed"]
+    tag = params["prepare"]["tag"]
+
+    random.seed(seed)
+
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+    create_directories([prepare_data_dir_path])
+
+    train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
+
+    encode = "utf8"
+    with open(source_data_path, encoding=encode) as fd_in: # actual input data that we are reading
+        with open(train_data_path, "w", encoding=encode) as fd_out_train: # writing train data
+            with open(test_data_path, "w", encoding=encode) as fd_out_test: # writing test data
+                process_posts(fd_in, fd_out_train, fd_out_test, tag, split)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
